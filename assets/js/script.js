3a88341e3a1952371b1680abc09946f160969c08
Noel Duggan <noelduggan21@gmail.com>
2024-06-03 19:30:52 +0000

    Add code to  calcuate to random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,44 @@
-function runGame(){
+
+document.addEventListener("DOMContentLoaded",function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for(let button of buttons){
+      button.addEventListener("click", function(){
+        if (this.getAttribute("data-type") === "submit"){
+            alert("You clicked Submit");
+        } else{
+            let gameType = this.getAttribute("data-type");
+            alert(`You clicked ${gameType}`);
+        }
+      })
+    }
+})
+/**
+ * THe main game loop 
+ */
+
+function runGame() {
+  let num1 =  Math.floor(Math.random() * 25) + 1;
+  let num2 = Math.floor(Math.random() * 25) + 1;
+}
+function checkAnswer() {
 
 }
-function checkAnswer(){
+function calculateCorrectAnswer() {
 
 }
-function calculateCorrectAnswer(){
+function incrementScore() {
 
 }
-function incrementScore(){
+function incrementWrongAnswer() {
 
 }
-function incrementWrongAnswer(){
+function displayAdditionQuestion() {
 
 }
-function displayAdditionQuestion(){
+function displaySubtractQuestion() {
 
 }
-function displaySubtractQuestion(){
-    
-}
-function displayMultiplyQuestion(){
-    
+function displayMultiplyQuestion() {
+
 }
